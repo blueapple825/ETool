9c3d22587033777a8502c396d6a9c1cd46f9643e
blueapple825 <blueapple8259@gmail.com>
2022-06-24 22:15:24 +0900

    :3

--- dist/inject.js
+++ dist/inject.js
@@ -8,11 +8,11 @@ function injectScript(src) {
     catch (e) {
     }
 }
-function injectCSS(src) {
+function injectCSS(href) {
     try {
         let tag = document.createElement("link");
         tag.setAttribute("rel", "stylesheet");
-        tag.setAttribute("src", src);
+        tag.setAttribute("href", href);
         document.getElementsByTagName("body")[0].appendChild(tag);
     }
     catch (e) {
--- src/inject.ts
+++ src/inject.ts
@@ -7,11 +7,11 @@ function injectScript(src:string){
     }
 }
 
-function injectCSS(src:string){
+function injectCSS(href:string){
     try{
         let tag=document.createElement("link");
         tag.setAttribute("rel","stylesheet");
-        tag.setAttribute("src",src);
+        tag.setAttribute("href",href);
         document.getElementsByTagName("body")[0].appendChild(tag);
     }catch(e){
     }
