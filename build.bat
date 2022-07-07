200c0184884117d25ba2079b5fcce4c485a92794
blueapple825 <blueapple8259@gmail.com>
2022-07-08 00:02:41 +0900

    :3

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 chcp 65001
+rmdir /s /q dist
+mkdir dist
 xcopy src\*.* dist /y /e /exclude:noCopy.txt
 echo 빌드중...
 tsc
--- src/modules/Events.ts
+++
@@ -1,1 +0,0 @@
-export const stopEvent=function(e:Event):boolean{e.preventDefault();e.stopPropagation();return false;}; //이벤트 삭제에 사용할 함수
